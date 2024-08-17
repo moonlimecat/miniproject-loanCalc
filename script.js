29b51385d3c89b88b4d740faa1f20844b3f3d8c1
moonlimecat <limepunch212@gmail.com>
2024-08-16 21:37:25 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const loanAmount = document.getElementById("inputLoan").value;
+const interest = document.getElementById("inputInterest").value;
+const paymentYear = document.getElementById("inputTermYear").value;
+const paymentMonth = document.getElementById("inputTermMonth").value;
+const submitBtn = document.getElementById("submitBtn");
+const resetBtn = document.getElementById("resetBtn");
