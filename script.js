da66e5a7feee57323645c5fdf6db882300526a5d
moonlimecat <limepunch212@gmail.com>
2024-08-16 23:32:01 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const loanAmount = document.getElementById("inputLoan").value;
-const interest = document.getElementById("inputInterest").value;
-const paymentYear = document.getElementById("inputTermYear").value;
-const paymentMonth = document.getElementById("inputTermMonth").value;
+let p;
+let r;
+let t;
+
 const submitBtn = document.getElementById("submitBtn");
 const resetBtn = document.getElementById("resetBtn");
+const inputs = document.querySelectorAll("input");
+
+function clear() {}
+
+submitBtn.addEventListener("click", function () {
+  p = document.getElementById("inputLoan").value;
+  p = Number(p);
+  r = document.getElementById("inputInterest").value;
+  r = Number(r);
+  t = document.getElementById("inputTermYear").value;
+  t = Number(t);
+  tm = document.getElementById("inputTermMonth").value;
+  tm = Number(tm) / 12;
+
+  z = (p * (r / 100 / 12)) / (1 - (1 + r / 100 / 12) ** (-12 * (t + tm)));
+
+  document.getElementById("payment").innerHTML =
+    `Payment: $` + Number(`${z}`).toFixed(2);
+});
+
+resetBtn.addEventListener("click", function () {
+  inputs.forEach((input) => (input.value = " "));
+});
